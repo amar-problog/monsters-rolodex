401dd6629a9d501849bcb69c721e86e02b392138
Amar Ismail <amar.pro.blog@gmail.com>
2020-02-25 18:46:38 +0300

    Added Card component feature

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Card } from '../card/card.component';
+
+
 import './card-list.styles.css';
+
+
 export const CardList =props => {
     console.log(props);
     return <div className='card-list'>
         {props.monsters.map(monsters => (
-            <h1 key={monsters.id}>{monsters.name}</h1>
-          ))}
+            <Card key={monsters.id} monster={monsters}/>
+        ))}
     </div>;
 };
