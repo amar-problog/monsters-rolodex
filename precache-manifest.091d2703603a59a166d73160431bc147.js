0bf711f5ba972b484544d19cdc690dbe10d4b791
Amar Ismail <amar.pro.blog@gmail.com>
2020-02-25 21:25:39 +0300

    Updates

---
+++ precache-manifest.091d2703603a59a166d73160431bc147.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "464466332179d71def0b97355f8c0c00",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "9635ebf47a5f2f6351ad",
+    "url": "/monsters-rolodex/static/css/main.c0ae091b.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9635ebf47a5f2f6351ad",
+    "url": "/monsters-rolodex/static/js/main.6ce96ac2.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
